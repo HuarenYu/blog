d1ea040597ad6130dce3818f22e3067d474c60dd
HuarenYU <yuhuarenproject@gmail.com>
2015-01-11 16:58:09 +0800

    blog initial commit

---
+++ configs.js
@@ -0,0 +1,22 @@
+var airpubConfigs = {
+  // blog name
+  name: '文若的博客',
+  // just a little description
+  description: '花满渚，酒满瓯',
+  // blog uri
+  url: 'http://huarenyu.github.io/blog/',
+  // blog theme
+  // theme: 'my-theme',
+  // for images upload
+  upyun: {
+    bucket: 'mybucket',
+    uri: 'http://{{bucketName}}.b0.upaiyun.com',
+    endpoint: 'http://v0.api.upyun.com/{{bucketName}}',
+    form_api_secret: 'xxxxxx'
+  }
+}
+
+// for backend service
+var duoshuoQuery = {
+  short_name: '{{wenruoblog}}'
+};
